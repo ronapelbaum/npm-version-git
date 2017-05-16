33193527fa31bce5aed20657b70d83577cf94ad0
Ron Apelbaum <ronapelbaum@gmail.com>
2017-05-16 12:32:38 +0300

    update index

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
-const npmVersionBump = require('./lib/npm-version-bump');
-
-const gitMsgRgx = /\[(major|minor|patch)\]/ig;
-const defaultSemver = 'patch';
-
-npmVersionBump(gitMsgRgx, defaultSemver);
+require('./lib/npm-version-bump')(gitMsgRgx, defaultSemver);
