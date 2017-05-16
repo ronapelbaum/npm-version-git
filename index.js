673a380c1581b8b39571d06e438a0843ec458f73
Ron Apelbaum <ronapelbaum@gmail.com>
2017-05-16 12:14:05 +0300

    refactor npm-version-bump

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
-const exec = require('child_process').exec;
+const npmVersionBump = require('./lib/npm-version-bump');
 
 const gitMsgRgx = /\[(major|minor|patch)\]/ig;
 const defaultSemver = 'patch';
 
-const gitLogExec = cb => exec('git log -1 --pretty=format:%s', cb);
-
-const parseRgx = (str, rgx, def) => (rgx.test(str) && rgx.exec(str)[1].toLowerCase()) || def;
-
-/* eslint no-unused-vars: "off" */
-gitLogExec((error, stdout, stderr) => {
-  /* eslint no-console: "off" */
-  console.log(parseRgx(stdout, gitMsgRgx, defaultSemver));
-});
+npmVersionBump(gitMsgRgx, defaultSemver);
